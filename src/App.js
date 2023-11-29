a5c316052351a1b97f88e8e2d85f4b36f4c742fa
minsangOh <flos9537@gmail.com>
2023-11-29 21:28:23 +0900

    섹션 2 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        complated: false,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        complated: false,
+      },
+      {
+        id: "3",
+        title: "밥먹기",
+        complated: false,
+      },
+    ],
+    value: "",
+  };
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = (complated) => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: complated ? "line-through": "none",
+    };
+  };
+
+  handClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    console.log(newTodoData);
+    this.setState({ todoData: newTodoData });
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+    console.log(e.target.value);
+  };
+
+  handlesubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      complated: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 추가
+    this.setState({ todoData: [...this.state.todoData, newTodo], value:"" });
+  };
+
+
+  handleClickChange = (id) => {
+    let newTodoData =this.state.todoData.map(data => {
+      if (data.id === id)
+      {
+        data.complated = !data.complated
+      }
+      return data
+    })
+
+    this.setState({todoData : newTodoData})
+  }
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+          {this.state.todoData.map((data) => (
+            <div style={this.getStyle(data.complated)} key={data.id}>
+              <input 
+              type="checkbox" 
+              defaultChecked={false} 
+              onChange={() => this.handleClickChange(data.id)}
+              />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handClick(data.id)}
+              >
+                x
+              </button>
+            </div>
+          ))}
+
+          <form style={{ display: "flex" }} onSubmit={this.handlesubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력 하세요"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
